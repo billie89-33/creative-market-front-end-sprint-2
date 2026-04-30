2d5dd038292ad4c4c78b4ecafa2984ceb48abfcb
Wathisa <wathisa@gmail.com>
2026-04-30 11:44:40 +0700

    feat: add initial ArtistProfile layout

--- src/pages/ArtistProfile.jsx
+++ src/pages/ArtistProfile.jsx
@@ -7,15 +7,17 @@ import ArtistProductGrid from "../components/ArtistProfile/06_ArtistProductGrid"
 
 const ArtistProfile = () => {
   return (
-    <main>
+    <main className="min-h-screen w-full bg-[#eeecfb]">
       <ArtistHeroBanner />
-      <section>
-        <div>
+
+      <section className="mx-auto grid max-w-7xl grid-cols-1 gap-10 px-4 pb-16 sm:px-6 md:px-8 lg:grid-cols-[0.95fr_1.35fr] lg:gap-14">
+        <div className="flex flex-col gap-8 lg:-mt-28">
           <ArtistProfileCard />
           <ArtistStats />
           <ArtistLinks />
         </div>
-        <div>
+
+        <div className="flex flex-col gap-8 pt-2 md:pt-4 lg:pt-12">
           <ArtistCategoryTabs />
           <ArtistProductGrid />
         </div>
