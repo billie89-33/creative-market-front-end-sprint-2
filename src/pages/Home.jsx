98e38134d9d1571f564e2a016f47e108276b20ed
AshaJenvasu <chaiyawatjenvasu@gmail.com>
2026-05-10 08:55:02 +0700

    Hide: Register Artist Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import ScrollingText from "../components/Home/02_ScrollingText";
 import Highlight from "../components/Home/03_Highlight";
 import Market from "../components/Home/04_Market";
 import ArtistLove from "../components/Home/05_Artist-love";
-import Register from "../components/Home/06_Register";
 import AboutUs from "../components/Home/07_AboutUs";
 
 import { useEffect } from "react";
@@ -28,7 +27,7 @@ const Home = () => {
       <Highlight />
       <Market />
       <ArtistLove />
-      <Register />
+
       <AboutUs />
     </main>
   );
